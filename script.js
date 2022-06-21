04a2e5ac4770c6ca934b7ed0ecd024d70266ba59
rachael <rachaelstevens92@gmail.com>
2022-06-21 16:40:13 +0100

    game now selecting instances of class spaceship and interacting with the DOM to reduce health points

---
+++ script.js
@@ -0,0 +1,66 @@
+const attackButton = document.querySelector(".attack-button");
+const resetButton = document.querySelector(".reset-button");
+// const motherShipHTMl = document.querySelector(".mothership");
+// const defenceShipHTML = document.querySelectorAll(".defence-ship");
+// const attackShipHTML = document.querySelectorAll(".attack-ship");
+
+// const test = () => console.log("working")
+// attackButton.addEventListener("click", test);
+
+//set up each type of ship as either a class or object
+//make sure the attack button randomly attacks each ship
+//create a reset button
+
+class spaceShip {
+    constructor(name, healthPoints, pointsWhenHit, domSelector) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.pointsWhenHit = pointsWhenHit;
+        this.domSelector = domSelector;
+    }
+    takeHP() {
+        this.healthPoints -= this.pointsWhenHit;
+    }
+    updateHTML() {
+        this.domSelector.innerHTML = `${this.name}<br>${this.healthPoints}hp`
+    }
+    deadSpaceship() {
+        if (this.healthPoints <= 0){
+            this.domSelector.innerHTML = "Spaceship Down!"
+        }
+    }
+}
+
+    const mothership = new spaceShip("Mothership", 100, 9, document.getElementById("mothership1"))
+    const defenceShip1 = new spaceShip("Defence Ship", 80, 10, document.getElementById("defence-ship-1"))
+    const defenceShip2 = new spaceShip("Defence Ship", 80, 10, document.getElementById("defence-ship-2"))
+    const defenceShip3 = new spaceShip("Defence Ship", 80, 10, document.getElementById("defence-ship-3"))
+    const defenceShip4 = new spaceShip("Defence Ship", 80, 10, document.getElementById("defence-ship-4"))
+    const defenceShip5 = new spaceShip("Defence Ship", 80, 10, document.getElementById("defence-ship-5"))
+    const attackShip1 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-1"))
+    const attackShip2 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-2"))
+    const attackShip3 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-3"))
+    const attackShip4 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-4"))
+    const attackShip5 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-5"))
+    const attackShip6 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-6"))
+    const attackShip7 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-7"))
+    const attackShip8 = new spaceShip("Attack Ship", 45, 12, document.getElementById("attack-ship-8"))
+
+    const spaceShipsArr = [mothership,defenceShip1,defenceShip2,defenceShip3,defenceShip4,defenceShip5,attackShip1,attackShip2,attackShip3,attackShip4,attackShip5,attackShip6,attackShip7,attackShip8]
+
+const attackSpaceShips = () => {
+
+    const chooseSpaceShip = () => {
+        const getRandomIndex = Math.floor(Math.random() * spaceShipsArr.length)
+        console.log(spaceShipsArr[getRandomIndex]);
+        return spaceShipsArr[getRandomIndex];
+    }
+const getSpaceShip = chooseSpaceShip();
+getSpaceShip.takeHP();
+getSpaceShip.updateHTML();
+getSpaceShip.deadSpaceship();
+}
+
+attackButton.addEventListener("click", attackSpaceShips);
+const reset = () => location.reload();
+resetButton.addEventListener("click", reset);
